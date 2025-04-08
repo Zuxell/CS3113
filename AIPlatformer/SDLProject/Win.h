74e01e7568862cce76949919963b74aa8bafc5a6
Zuxell <138546174+Zuxell@users.noreply.github.com>
2025-04-07 22:57:34 -0400

    ai project

---
+++ AIPlatformer/SDLProject/Win.h
@@ -0,0 +1,15 @@
+#include "Win.h"
+
+class Win : public Scene {
+public:
+    // ————— STATIC ATTRIBUTES ————— //
+    int ENEMY_COUNT = 1;
+    
+    // ————— DESTRUCTOR ————— //
+    ~Win();
+    
+    // ————— METHODS ————— //
+    void initialise() override;
+    void update(float delta_time) override;
+    void render(ShaderProgram *program) override;
+};
